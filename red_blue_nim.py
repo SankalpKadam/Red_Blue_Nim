b21afa07c56e26560d5d76daf216043464478a0e
Sankalp Kadam <sankalptp07@gmail.com>
2023-04-09 02:07:29 -0500

    Added comments for code

--- red_blue_nim.py
+++ red_blue_nim.py
@@ -1,93 +1,99 @@
 #References - https://www.geeksforgeeks.org/python-infinity/ , https://www.youtube.com/watch?v=zp3VMe0Jpf8
 import argparse
 
+# Each node in tree is generated using this class.
 class State():
-    def __init__(self,red_stones, blue_stones,parent_state,alpha,beta,action):
-        self.red_stones=red_stones
-        self.blue_stones=blue_stones
-        self.parent_state=parent_state
-        self.action=action
-        self.alpha=alpha
-        self.beta=beta
+    def __init__(self,red_stones, blue_stones,action):
+        self.red_stones = red_stones
+        self.blue_stones = blue_stones
+        self.action = action
     def return_data(self):
         return f"Red: {self.red_stones}, Blue: {self.blue_stones}, parent_state: {self.parent_state}, action : {self.action}"
 
+#Utility function to calculate values of terminal states.
 def utility_function(state, caller):
     if caller == "max":
         return 2*state.red_stones + 3*state.blue_stones
     else:
         return -(2*state.red_stones + 3*state.blue_stones)
 
+#Generates successors for current state.
 def possible_actions(state):
-    possible_states=list()
-    take_from_red=State(state.red_stones-1,state.blue_stones,state.return_data(),float('inf'),float('inf'),"Take from red")
+    possible_states = list()
+    take_from_red = State(state.red_stones-1,state.blue_stones,"Take from red")
     possible_states.append(take_from_red)
-    take_from_blue=State(state.red_stones,state.blue_stones-1,state.return_data(),float('inf'),float('inf'),"Take from blue")
+    take_from_blue = State(state.red_stones,state.blue_stones-1,"Take from blue")
     possible_states.append(take_from_blue)
     return possible_states
 
+#Maximizes the value for result of performing actions on current state.
 def alpha_beta_decision(state):
+    alpha = float('-inf')
+    beta = float('inf')
     successors_of_current_State = possible_actions(state)
-    current_winning=float('-inf')
-    winning_state=successors_of_current_State[0]
+    current_winning = float('-inf')
+    winning_state = successors_of_current_State[0]
     for s in successors_of_current_State:
-        v = Min_Value(s,float('-inf'),float('inf'))
+        v = Min_Value(s,alpha,beta)
+        alpha = v
         if v > current_winning:
-            current_winning=v
-            winning_state=s
+            current_winning = v
+            winning_state = s
     return winning_state
 
+#Max node in minmax.
 def Max_Value(state,alpha,beta):
-    if state.red_stones==0 or state.blue_stones==0:
+    #Check for terminal state
+    if state.red_stones == 0 or state.blue_stones == 0:
         return utility_function(state,"max")
-    v= float('-inf') #Assign infinity value to v
+    v = float('-inf') #Assign infinity value to v
     #find successors of s
-    successors_of_given_state=possible_actions(state)
-
+    successors_of_given_state = possible_actions(state)
     for s in successors_of_given_state:
-        mv = Min_Value(s,alpha,beta)
-        if v < mv:
-            v = mv
-        if mv >= beta:
+        current_value = Min_Value(s,alpha,beta)
+        if v < current_value:
+            v = current_value
+        if current_value >= beta:
             return v
-        if mv > alpha:
-            alpha = mv
+        if current_value > alpha:
+            alpha = current_value
     return v
 
+#Min node in minmax.
 def Min_Value(state,alpha,beta):
-    if state.red_stones==0 or state.blue_stones==0:
+    #Check for terminal state
+    if state.red_stones == 0 or state.blue_stones == 0:
         return utility_function(state,"min")
-    v= float('inf') #Assign infinity value to v
+    v = float('inf') #Assign infinity value to v
     #find successors of s
-    successors_of_given_state=possible_actions(state)
-
+    successors_of_given_state = possible_actions(state)
     for s in successors_of_given_state:
-        mv = Max_Value(s,alpha,beta)
-        if mv < v:
-            v=mv
-        if mv<=alpha:
+        current_value = Max_Value(s,alpha,beta)
+        if current_value < v:
+            v = current_value
+        if current_value <= alpha:
             return v
-        if mv < beta:
-            beta = mv
+        if current_value < beta:
+            beta = current_value
     return v
 
-
+#Perform minmax for current game state.
 def Do_Min_Max(current_state):
     move = alpha_beta_decision(current_state)
     return move.action
 
+#Perform computer move after deciding using minmax.
 def Make_Computer_Move(current_state, move_to_be_done):
     if move_to_be_done == "Take from red":
         print("Computer took a red ball")
         current_state.red_stones -= 1
-        # return current_state
     elif move_to_be_done == "Take from blue":
         print("Computer took a blue ball")
         current_state.blue_stones -= 1
-        # return current_state
     print(f"\nCurrent state is: Red Balls {current_state.red_stones} Blue Balls {current_state.blue_stones}\n")
     return current_state
 
+#Check if game is over.
 def Check_If_Game_Over(game_state, player):
     if (game_state.red_stones == 0 or game_state.blue_stones == 0) and player == "Human":
         print(f"Computer won with score : {game_state.red_stones*2 + game_state.blue_stones*3}")
@@ -98,10 +104,12 @@ def Check_If_Game_Over(game_state, player):
     else:
         return False
 
+#Get move from human.
 def Get_Human_Move():
     human_move = input("Enter color to pick either red or blue ball: ")
     return human_move.lower()
 
+#Perform move given by human.
 def Perform_Human_Move(current_state, move_to_be_done):
     if move_to_be_done == "red":
         current_state.red_stones -= 1
@@ -115,8 +123,7 @@ def Perform_Human_Move(current_state, move_to_be_done):
         move_to_be_done = input("Enter a valid color: ")
         return Perform_Human_Move(current_state, move_to_be_done)
         
-
-
+#Executed if computer is the first player.
 def computer_plays_first(initial_state):
     updated_game_state_by_human = None
     while True:
@@ -132,7 +139,9 @@ def computer_plays_first(initial_state):
         updated_game_state_by_human = Perform_Human_Move(updated_game_state_by_computer, move_selected_by_human)
         if Check_If_Game_Over(updated_game_state_by_human, "Human"):
             break
+        print("--------------------------------------------")
 
+#Executed if human is the first player.
 def human_plays_first(initial_state):
     updated_game_state_by_computer=None
     while True:
@@ -147,6 +156,7 @@ def human_plays_first(initial_state):
         updated_game_state_by_computer = Make_Computer_Move(updated_game_state_by_human, move_selected_by_computer)
         if Check_If_Game_Over(updated_game_state_by_computer, "Computer"):
             break
+        print("---------------------------------------------")
 
 
 if __name__ == "__main__":
@@ -157,7 +167,8 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--player", default="computer",
                         help="Use this option to specify who plays first")
     commandline_args = parser.parse_args()
-    initial_state=State(commandline_args.red_stones,commandline_args.blue_stones,None,float('-inf'),float('inf'),None)
+    initial_state=State(commandline_args.red_stones,commandline_args.blue_stones,None)
+    print("---------------------------------------------")
     print(f"Initial State is : Red {initial_state.red_stones} Blue {initial_state.blue_stones}\n")
     if commandline_args.player.lower() == "computer":
         computer_plays_first(initial_state)
